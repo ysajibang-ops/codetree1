519f8416e98e345b225c79819a9ea0bc3a0e5a16
ysajibang-ops <ysajibang@gmail.com>
2026-08-22 22:52:13 +0900

    [Passed] 달에서 무게 구하기 (46ms, 16MB)

---
+++ trail0/달에서 무게 구하기/weight-on-the-moon.py
@@ -0,0 +1,9 @@
+anrp,rate=13,0.165
+print(f"{anrp} * {rate:.6f} = {anrp*rate:.6f}")
+
+# anrp,rate=13,0.165
+# print("%d * %.6f = %.6f" % (anrp,rate,anrp*rate))
+
+# anrp,rate=13,0.165
+# rhq=anrp*rate
+# print("{0} * {1:.6f} = {2:.6f}".format(anrp,rate,rhq))
