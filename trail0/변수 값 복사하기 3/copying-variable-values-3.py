8de8be957d43a2c0406e314bc59c3e9afe06beb3
ysajibang-ops <ysajibang@gmail.com>
2026-08-22 22:45:21 +0900

    [Passed] 변수 값 복사하기 3 (50ms, 16MB)

---
+++ trail0/변수 값 복사하기 3/copying-variable-values-3.py
@@ -0,0 +1,23 @@
+a,b,c=1,5,3
+a=c
+a=a+c
+b=b-c
+print(a,b,c,sep='\n')
+
+
+
+
+
+# 줄바꿈 안해서 틀림
+# a,b,c=1,5,3
+# a=c
+# a=a+c
+# b=b-c
+# print(f"{a},{b},{c}")
+
+# 줄바꿈 한 버전
+# a,b,c=1,5,3
+# a=c
+# a=a+c
+# b=b-c
+# print(a,b,c,sep='\n')
