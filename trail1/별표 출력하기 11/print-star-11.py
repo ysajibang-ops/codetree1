989f8e3172ebc3352ff677576306bb63a2948098
ysajibang-ops <ysajibang@gmail.com>
2026-08-24 21:50:32 +0900

    [Passed] 별표 출력하기 11 (53ms, 16MB)

---
+++ trail1/별표 출력하기 11/print-star-11.py
@@ -0,0 +1,12 @@
+N=int(input())
+
+
+for i in range(2*N+1):
+    
+    for j in range(2*N+1):
+        if i%2==0 or j%2==0:
+            print("*", end=" ")
+        
+        else:
+            print(" ", end=" ")
+    print()
