d4e45dbac5dc597f28b03045038d30ad2ca29a53
ysajibang-ops <ysajibang@gmail.com>
2026-08-25 21:23:45 +0900

    [Passed] 순서쌍 만들기 (50ms, 16MB)

--- trail1/순서쌍 만들기/making-order-pair.py
+++ trail1/순서쌍 만들기/making-order-pair.py
@@ -5,3 +5,13 @@ for i in range(N):
     for j in range(N):
         print(f"({N-i},{N-j})",end=" ")
     print()
+
+
+
+# 다른아이디어
+# n= int(input())
+
+# for i in range(n,0,-1):
+#     for j in range(n,0,-1):
+#         print(f'({i},{j})', end=' ')
+#     print()
