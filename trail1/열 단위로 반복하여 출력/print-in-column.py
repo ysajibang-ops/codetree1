505a1381a893aa18328333446444c90535cdba41
ysajibang-ops <ysajibang@gmail.com>
2026-08-25 21:17:37 +0900

    [Passed] 열 단위로 반복하여 출력 (54ms, 16MB)

--- trail1/열 단위로 반복하여 출력/print-in-column.py
+++ trail1/열 단위로 반복하여 출력/print-in-column.py
@@ -1,8 +1,15 @@
 N=int(input())
 
-for i in range(N):
+cnt=1
 
-    for j in range(N):
-        print(j+1,j+1,j+1, sep="")
+for i in range(1,N+1):
 
+    for j in range(1,N+1):
+        print(i, end="")
     print()
+
+# 더 간단한 방법
+# N = int(input())
+
+# for i in range(1, N+1):
+#     print(str(i) * N)
